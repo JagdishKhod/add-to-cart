0cc53cbc2e3cb8b4dbccca2d7d72d43f7f0143e4
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-10 19:56:38 +0530

    to increase count on cart

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -16,7 +16,11 @@ ngOnInit(): void{
 this.api.getProduct()
 .subscribe(res=>{
   this.productList = res;
-})
+
+  this.productList.forEach((a: any) => {
+    Object.assign(a,{quantity:1,total:a.price});
+  });
+ })
 }
 addtocart(item : any){
   this.cartService.addtoCart(item);
