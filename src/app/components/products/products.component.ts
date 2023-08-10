1d6b33edf96bc9ce45ed5c22185947eb90ead38c
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-10 18:54:47 +0530

    addtocart method in product.ts

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiService } from 'src/app/service/api.service';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -9,12 +10,15 @@ import { ApiService } from 'src/app/service/api.service';
 export class ProductsComponent implements OnInit {
 
 public productList : any ;
-constructor(private api : ApiService){ }
+constructor(private api : ApiService, private cartService : CartService){ }
 
 ngOnInit(): void{
 this.api.getProduct()
 .subscribe(res=>{
   this.productList = res;
 })
+}
+addtocart(item : any){
+  this.cartService.addtoCart(item);
 }
 }
