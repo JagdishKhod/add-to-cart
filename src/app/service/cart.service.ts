675a099a07a9cd3a0c25c1e38d31c91393f7b986
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-10 18:41:26 +0530

    create getProduct, setProductList, addtoCart, getTotalPrice methods

---
+++ src/app/service/cart.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CartService {
+  public cartItemList : any =[]
+  public productList = new BehaviorSubject<any>([]);
+
+  constructor() { }
+  getProducts(){
+    return this.productList.asObservable();
+  }
+  
+  setProductList(product: any){
+    this.cartItemList.push(...product);
+    this.productList.next(product);
+  }
+  addtoCart(product:any){
+    this.cartItemList.push(product);
+    this.productList.next(this.cartItemList);
+    this.getTotalPrice();
+  }
+  getTotalPrice(){
+    let grandTotal=0;
+    this.cartItemList.map((a:any)=>{
+      grandTotal += a.total;
+    })
+  }
+}
