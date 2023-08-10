4940e9f725bd833f638a29f515fccf0ed23c202c
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-10 11:48:50 +0530

    added items img, desc, and price ..used interpolation

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,16 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from 'src/app/service/api.service';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.css']
 })
-export class ProductsComponent {
+export class ProductsComponent implements OnInit {
 
-  // publicProductLis
-constructor(){ }
+public productList : any ;
+constructor(private api : ApiService){ }
 
-ngOnInIt(): void{
-
+ngOnInit(): void{
+this.api.getProduct()
+.subscribe(res=>{
+  this.productList = res;
+})
 }
 }
