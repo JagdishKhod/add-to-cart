4c397410e2e284c5495684686cccb134f07246eb
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-12 20:11:18 +0530

    Done Successfull

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,6 +10,7 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
 
 public productList : any ;
+public filterCategory :any;
 searchKey:string ="";
 constructor(private api : ApiService, private cartService : CartService){ }
 
@@ -17,10 +18,14 @@ ngOnInit(): void{
 this.api.getProduct()
 .subscribe(res=>{
   this.productList = res;
-
+  this.filterCategory = res;
   this.productList.forEach((a: any) => {
+    if(a.category === "women's clothing" || a.category ==="men's clothing"){
+      a.category="fashion"
+    }
     Object.assign(a,{quantity:1,total:a.price});
   });
+  console.log(this.productList)
  });
 
  this.cartService.search.subscribe((val:any)=>{
@@ -29,5 +34,13 @@ this.api.getProduct()
 }
 addtocart(item : any){
   this.cartService.addtoCart(item);
+}
+filter  (category:string){
+  this.filterCategory =this.productList
+  .filter((a:any)=>{
+    if(a.category==category || category==''){
+      return a;
+    }
+  })
 }
 }
