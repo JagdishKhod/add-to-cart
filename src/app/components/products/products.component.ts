6184c61e5916095d17c66c93a9d678e521ba0855
jagdishkhod <jagdishkhod892@gmail.com>
2023-08-12 18:54:09 +0530

    search bar Builded successful

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -10,6 +10,7 @@ import { CartService } from 'src/app/service/cart.service';
 export class ProductsComponent implements OnInit {
 
 public productList : any ;
+searchKey:string ="";
 constructor(private api : ApiService, private cartService : CartService){ }
 
 ngOnInit(): void{
@@ -20,6 +21,10 @@ this.api.getProduct()
   this.productList.forEach((a: any) => {
     Object.assign(a,{quantity:1,total:a.price});
   });
+ });
+
+ this.cartService.search.subscribe((val:any)=>{
+  this.searchKey = val;
  })
 }
 addtocart(item : any){
